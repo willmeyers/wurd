d933338d46fb77881650d05418b614739924b3e1
Will Meyers <williamgmeyers@gmail.com>
2018-01-12 13:58:14 -0500

    Pretty much everything is working.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,13 @@
-import pyperclip
 import argparse as arg
-from password import Password
-from db import NewWurdInstance, WurdInstance
+from wurd import NewWurdInstance, WurdInstance
 
     
 def main():
     parser = arg.ArgumentParser()
 
-    parser.add_argument('-i', '--init', help='Initialize a new Wurd Storage instance')
-    parser.add_argument('-n', '--new', help='Creates a new password.')
-    parser.add_argument('-f', '--fetch', help='Fetch a password.')
-    parser.add_argument('-c', '--change', help='Change password to a custom password.')
+    parser.add_argument('-i', '--init', help='Initializes a new Wurd Storage instance')
+    parser.add_argument('-n', '--new', help='Creates a new password or updates an old one.')
+    parser.add_argument('-g', '--get', help='Fetches a password.')
     parser.add_argument('-d', '--delete', help='Deletes a password.')
 
     args = vars(parser.parse_args())
@@ -32,35 +29,17 @@ def main():
     elif args['new']:
         name = args['new']
         wurd = WurdInstance()
+        p = wurd.generate_password()
+        wurd.save_password(name, p)
 
-        passwd = Password(name).generate()
-        
-        print('Your new password for %s: %s' % (name, passwd))
-        conf = input('Save this password? (yes/no) ')
-
-        if conf.lower() == 'yes':
-            pyperclip.copy(passwd)
-            wurd.save_password(name, passwd)
-            print('Password copied to clipboard.')
-        else:
-            print('Password discared.')
-
-    elif args['fetch']:
-        name = args['fetch']
+    elif args['get']:
+        name = args['get']
         wurd = WurdInstance()
-        passwd = wurd.fetch_password(name)
-        print('Your password: ', passwd)
-        pyperclip.copy(passwd)
-        print('has been saved to your clipboard.')
-
-    elif args['change']:
-        pass
-
+        wurd.get_password(name)
+        
     elif args['delete']:
-        pass
-    
-    else:
-        print('No valid arguments given!')
+        wurd = WurdInstance()
+        wurd.delete_password(args['delete'])
         
         
 if __name__ == '__main__':
--- src/wurd.py
+++ src/wurd.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import random
+import pyperclip
 import sqlite3
 import yaml
 from getpass import getpass
@@ -16,21 +18,8 @@ SCHEMA = ('CREATE TABLE IF NOT EXISTS passwords ('
           'id INTEGER PRIMARY KEY,'
           'name TEXT NOT NULL,'
           'created_on TEXT NOT NULL,'
-          'updated_on TEXT NOT NULL,'
           'password TEXT NOT NULL'
           ');'
-)
-
-
-INSERT_PASSWD = ('INSERT INTO passwords ('
-                 'name,'
-                 'created_on,'
-                 'updated_on,'
-                 'password'
-                 ')'
-                 'VALUES ('
-                 '{0}, {1}, {2}, {3}'
-                 ');'
 )
 
 
@@ -39,9 +28,13 @@ class NewWurdInstance:
         self.name = name + '.db'
         self.wurd_path = self.create_wurd_directory()
         
+        self.create_config()
+
+        with open(os.path.join(self.wurd_path, 'config.yml'), 'r') as f:
+            self.config = yaml.load(f)
+        
         self.create_database()
         self.create_keys()
-        self.create_config()
         
     def create_wurd_directory(self):
         ho = os.path.expanduser('~')
@@ -108,13 +101,13 @@ class NewWurdInstance:
             format = serialization.PublicFormat.SubjectPublicKeyInfo
         )
         
-        with open(os.path.join(self.wurd_path, 'wurd_private_key.pem'), 'wb+') as f:
+        with open(os.path.join(self.config['keys']['private key']), 'wb+') as f:
             print('Writing private key to', self.wurd_path)
             for line in pri_pem.splitlines():
                 l = line + '\n'.encode()
                 f.write(l)
 
-        with open(os.path.join(self.wurd_path, 'wurd_public_key.pem'), 'wb+') as f:
+        with open(os.path.join(self.config['keys']['public key']), 'wb+') as f:
             print('Writing public key to', self.wurd_path)
             for line in pub_pem.splitlines():
                 l = line + '\n'.encode()
@@ -131,11 +124,6 @@ class NewWurdInstance:
             'keys': {
                 'private key': os.path.join(self.wurd_path, 'wurd_private_key.pem'),
                 'public key': os.path.join(self.wurd_path, 'wurd_public_key.pem'),
-            },
-            'log': {
-                'last accessed': datetime.now(),
-                'last updated': None,
-                'last command': None
             }
         }
         
@@ -151,6 +139,9 @@ class WurdInstance:
 
         with open(os.path.join(self.wurd_path, 'config.yml'), 'r') as f:
             self.config = yaml.load(f)
+            
+        self.conn = self.connect()
+        self.cur = self.conn.cursor()
         
     def connect(self):
         db_name = self.config['database']['name']
@@ -165,8 +156,7 @@ class WurdInstance:
         return conn
     
     def load_private_key(self):
-        with open(os.path.join(self.wurd_path, 'wurd_private_key.pem'), 'rb') as k:
-            print('Reading private key from', self.wurd_path)
+        with open(os.path.join(self.config['keys']['private key']), 'rb') as k:
             pri_key = serialization.load_pem_private_key(
                 k.read(),
                 password = getpass('Enter your master passphrase:').encode(),
@@ -176,8 +166,7 @@ class WurdInstance:
         return pri_key
 
     def load_public_key(self):
-        with open(os.path.join(self.wurd_path, 'wurd_public_key.pem'), 'rb') as k:
-            print('Reading public key from', self.wurd_path)
+        with open(os.path.join(self.config['keys']['public key']), 'rb') as k:
             pub_key = serialization.load_pem_public_key(
                 k.read(),
                 backend = default_backend()
@@ -186,10 +175,51 @@ class WurdInstance:
         return pub_key
     
     def save_password(self, name, password):
-        conn = self.connect()
-
         pub_key = self.load_public_key()
+        ciph = self.encrypt_password(password, pub_key)
+        
+        now = datetime.now().strftime('%d-%m-%Y %H:%M:%S%p')
 
+        if self.search(name) is not None:
+            print('A password with that name already exists.')
+            sys.exit(1)
+            
+        self.cur.execute('INSERT INTO passwords (name, created_on, password) VALUES (?,?,?);', (name, now, ciph))
+        self.conn.commit()
+        print('Inserted password for %s into wurd database on %s.' % (name, now))
+
+    def get_password(self, name):
+        pri_key = self.load_private_key()
+
+        s = self.search(name)
+        if s is None:
+            print('No pss')
+            sys.exit()
+        
+        plain = pri_key.decrypt(
+            s[0],
+            padding.OAEP(
+                mgf = padding.MGF1(algorithm=hashes.SHA1()),
+                algorithm = hashes.SHA1(),
+                label = None
+            )
+        )
+
+        pyperclip.copy(plain.decode())
+        print('Fetched password for %s and saved to clipboard.' % name)
+
+        
+    def delete_password(self, name):
+        if self.search(name) is None:
+            print('No passwords with that name.')
+            sys.exit(1)
+
+        self.cur.execute('DELETE FROM passwords WHERE name=(?);', (name,))
+        self.conn.commit()
+
+        print('Successfully deleted password for', name)
+        
+    def encrypt_password(self, password, pub_key):
         print('Encrypting password using public key.')
         ciph = pub_key.encrypt(
             password.encode(),
@@ -200,48 +230,24 @@ class WurdInstance:
             )
         )
 
-        cur = conn.cursor()
-        insert_sql = ('INSERT INTO passwords ('
-                      'name,'
-                      'created_on,'
-                      'updated_on,'
-                      'password'
-                      ')'
-                      'VALUES (?,?,?,?);'
+        return ciph
+    
+    def generate_password(self):
+        allowed = ('abcdefghijklmnopqrstuvwxyz'
+                   'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                   '0123456789!@#$%^&*()-+_=,.<>?:;{}[]~'
         )
-
-        now = datetime.now().strftime('%d-%m-%Y %H:%M:%S%p')
-        cur.execute(insert_sql, (name, now, now, ciph))
+        p = []
         
-        print('Inserted password for %s into wurd database on %s.' % (name, now))
+        for c in range(16):
+            p.append(random.choice(allowed))
+            random.shuffle(p)
 
-        conn.commit()
-        conn.close()
-
-    def fetch_password(self, name):
-        conn = self.connect()
-        cur = conn.cursor()
-
-        pri_key = self.load_private_key()
-
-        print('Fetching password...')
-        p = cur.execute('SELECT password FROM passwords WHERE name = \'%s\'' % name)
-        p = p.fetchone()[0]
-
-        plain = pri_key.decrypt(
-            p,
-            padding.OAEP(
-                mgf = padding.MGF1(algorithm=hashes.SHA1()),
-                algorithm = hashes.SHA1(),
-                label = None
-            )
-        )
-
-        print('Done.')
+        p = ''.join(p)
+        pyperclip.copy(p)
+        print('Password was saved to your clipboard.')
+        return p
         
-        conn.close()
-
-        return plain.decode()
-        
-    def delete_password(self, name):
-        pass
+    def search(self, name):
+        return self.cur.execute('SELECT password FROM passwords WHERE name =(?);', (name,)).fetchone()
+                        
